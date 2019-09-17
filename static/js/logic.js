2591d0cea3f8d0b844500b399eea5b07f21ae9a8
dpkeyes <dpkeyes@gmail.com>
2019-09-17 11:41:08 -0700

    done except for popup issue

--- static/js/logic.js
+++ static/js/logic.js
@@ -21,6 +21,9 @@ function date(date) {
     return date.toLocaleString();
 };
 
+// Define list of colors to represent earthquakes of magnitudes 0-1, 1-2, 2-3, 3-4, 4-5, or 5+
+var colors = ["#87EE77", "#B3EE77", "#D8EE77", "#EECA77", "#EEA477", "#EE8477"];
+
 // Grab data from API endpoint using d3
 d3.json(url, function(response) {
 
@@ -42,27 +45,27 @@ d3.json(url, function(response) {
     var radius = "";
 
     if (magnitude < 1) {
-      color = "#87EE77";
+      color = colors[0];
       radius = 5;
     }
     else if (magnitude < 2) {
-      color = "#B3EE77";
+      color = colors[1];
       radius = 10;
     }
     else if (magnitude < 3) {
-      color = "#D8EE77";
+      color = colors[2];
       radius = 15;
     }
     else if (magnitude < 4) {
-      color = "#EECA77";
+      color = colors[3];
       radius = 20;
     }
     else if (magnitude < 5) {
-      color = "#EEA477";
+      color = colors[4];
       radius = 25;
     }
     else {
-      color = "#EE8477";
+      color = colors[5];
       radius = 30;
     };
 
@@ -70,13 +73,52 @@ d3.json(url, function(response) {
     if (location) {
 
       // Add a new marker to the cluster group and bind a pop-up
-      var circle = L.circleMarker([location.coordinates[1], location.coordinates[0]], {
+      L.circleMarker([location.coordinates[1], location.coordinates[0]], {
         color: color,
+        fillColor: color,
+        fillOpacity: 0.8,
         radius: radius
       }).bindPopup("<h3>Magnitude: " + magnitude + "</h3><h5>Timestamp: " + date(timestamp) + "</h5>")
         .addTo(myMap);
     };
   };
+
+  // Create legend mapping colors to earthquakes of different magnitudes
+  // --------------------------------------------------------------------
+ 
+  // Create legend variable and place it in bottom right part of map
+  var legend = L.control({position: "bottomright"});
+
+  // Define function that adds legend to the html
+  legend.onAdd = function (map) {
+
+    // Define div to house the legend
+    var div = L.DomUtil.create("div", "legend");
+
+    // Define categories list of different magnitude buckets
+    var categories = ["0-1", "1-2", "2-3", "3-4", "4-5", "5+"];
+
+    // Define list with table title to house inner html of div
+    var html = ["<table style=background-color:gray><tr><th>Magnitude</tr></th>"];
+
+    // Push remaining html elements to html list using for loop
+    for (var i = 0; i < categories.length; i++) {
+        html.push(
+            "<tr><td>" + categories[i] + "</td><td><svg width='20' height='10'><rect x='0' y='0' width='20' height='10' fill='" + colors[i] + "'</></svg></td></tr>"
+        );
+    };
+
+    // Push final closing table tag
+    html.push("</table>")
+
+    // Join elements of html list together to form final html
+    div.innerHTML = html.join("");
+
+    return div;
+  };
+
+  // Call the legend.onAdd function with the input parameter myMap
+  legend.addTo(myMap);
 
 });
 
